1e6c43c99d71235c041e11ec8b9737559988ae7e
Anudita Keshri <anudita1999@gmail.com>
2021-08-04 23:46:09 +0530

    initial commit

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=E:\SDK\flutter"
-export "FLUTTER_APPLICATION_PATH=E:\Anudita_Flutter_course\bmi-calculator-flutter"
+export "FLUTTER_APPLICATION_PATH=E:\Anudita_Flutter_course\my_flutter_apps\bmi_calc_app\bmi-calculator-flutter"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
